38cc502c43ac2f215b6950c6c60a63574e35ba0e
mengyanggithub <mengyang910804@gmail.com>
2022-07-06 16:03:34 +0800

    bug fix

--- examples/wenetspeech/s0/generate_testset.py
+++ examples/wenetspeech/s0/generate_testset.py
@@ -14,7 +14,7 @@ with open("./data/test/wav_docker.scp", "w") as f_out:
 
 new_lines = []
 for line in tqdm(lines):
-    new_line = line.replace("/work/zhangjincheng01/asr/data/test/test_v20220321/","/work/yangmeng03/wenet/examples/wenetspeech/s0/data/test")
+    new_line = line.replace("/work/zhangjincheng01/asr/data/test/test_v20220321/","/work/yangmeng03/wenet/examples/wenetspeech/s0/data/test/")
     new_lines.append(new_line)
 
 with open("./data/test/wav_new.scp", "w") as f_out:
