2ee0648e431272cab3afe1e84e836a4b86ebc954
mengyanggithub <mengyang910804@gmail.com>
2022-07-08 10:13:20 +0800

    bug fix

--- examples/wenetspeech/s0/analysis.py
+++ examples/wenetspeech/s0/analysis.py
@@ -14,7 +14,7 @@ def get_error_file(in_file,in_file_refer,out_file):
             new_lines.append(line)
             new_lines.append(lines[i+1])
             new_lines.append(lines[i+2])
-            if lines[i+1] != lines_refer[i+1]:
+            if lines[i+2] != lines_refer[i+2]:
                 new_lines.append(lines_refer[i+1])
             new_lines.append("\n")
     
